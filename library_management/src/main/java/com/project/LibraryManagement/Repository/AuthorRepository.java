17888ba08d0729438bd90a62d74584f9acb15389
Megha Emaley <81461227+MeghaEmaley@users.noreply.github.com>
2023-05-22 13:17:29 +0530

    Delete library_management directory

--- library_management/src/main/java/com/project/LibraryManagement/Repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.LibraryManagement.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.LibraryManagement.Entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
